a0fcb510f64e9c88a0b41b24e6c573bfa78d52c7
jgonbur142 <jgonbur142@g.educaand.es>
2025-12-10 08:43:02 +0100

    Tercera versión que añade println RESULTADO en el main de la clase Factorial

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -13,6 +13,8 @@ public class Factorial {
         } else {
             System.out.println("Has introducido el número: " + num);
         }
+        
+        System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
 
         
 	}
