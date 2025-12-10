e9c5053893108fc11a5b300d70372e2a91d3eb9f
jgonbur142 <jgonbur142@g.educaand.es>
2025-12-10 09:37:11 +0100

    Revert "Versión F4 que añade println proceso completado al final del main de la clase Factorial"
    This reverts commit 53d84488d8a3bfa9125310e0a49ee3118c5436c9.

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -15,7 +15,6 @@ public class Factorial {
         }
        
         System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialRec(num));
-        System.out.println("Proceso completado usando el método recursivo.");
 
         
 	}
